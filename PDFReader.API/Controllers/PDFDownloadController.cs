a74af0f88a9b52c6b9407e35de722eda31882048
Brook-B-Nigatu <burabalke7776@gmail.com>
2024-08-13 10:45:54 +0200

    renamed projects and cleaned up weather forecast code

---
+++ PDFReader.API/Controllers/PDFDownloadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PDFReader.API.Controllers
+{
+    [Route("api/download")]
+    [ApiController]
+    public class PDFDownloadController : ControllerBase
+    {
+        [HttpGet("")]
+        public string[] GetList()
+        {
+            // Return an array of the available files
+            string[] fileNames = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "Files"));
+
+            return fileNames.Select(s => Path.GetFileName(s)).ToArray();
+        }
+
+        [HttpGet("{fileName}")]
+        public IActionResult GetPDF(string fileName)
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "Files", fileName), "application/pdf", fileName);
+        }
+
+    }
+
+
+}
